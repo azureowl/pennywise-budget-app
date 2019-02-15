db4e8668de2550cb0691fb5630e07e45b0c72984
Azureowl <maiaroche16@gmail.com>
2019-02-15 13:04:20 -0800

    fixed style on overlay menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,13 +4,19 @@ import './Nav.css'
 export default function Nav(props) {
     return (
         <nav className={props.class}>
-            <ul>
-                <li><a href="/">Login</a></li>
-                <li><a href="/">How it works</a></li>
-                <li><a href="https://www.nerdwallet.com/the-best-credit-cards?trk=nw_gn2_4.0">Best Credit Cards</a></li>
-                <li><a href="https://www.thebalance.com/reasons-to-budget-money-2385699">Why Budget?</a></li>
-                <li><a className="sign-up" href="/">Sign Up</a></li>
-            </ul>
+            <div className="info-links">
+                <ul>
+                    <li><a href="/">How it works</a></li>
+                    <li><a href="https://www.nerdwallet.com/the-best-credit-cards?trk=nw_gn2_4.0">Best Credit Cards</a></li>
+                    <li><a href="https://www.thebalance.com/reasons-to-budget-money-2385699">Why Budget?</a></li>
+                </ul>
+            </div>
+            <div className="login-links">
+                <ul>
+                    <li><a className="cta" href="/">Login</a></li>
+                    <li><a className="cta" href="/">Sign Up</a></li>
+                </ul>
+            </div>
         </nav>
     );
 }
