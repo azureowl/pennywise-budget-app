6ca70b4220468efbff59e0c948914bf002a9cd7f
Azureowl <maiaroche16@gmail.com>
2019-03-19 13:26:41 -0700

    Added image to features

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import MainButton from './MainButton';
+import budget from './img-budget.png';
+import budgetMB from './img-budget_mb.png';
+import track from './img-track.png';
+import trackMB from './img-track_mb.png';
+import flex from './img-flex.png';
+import flexMB from './img-flex_mb.png';
 
 export default function Features () {
-    let messages = [['Budgets? Yes!', 'fas fa-money-check-alt', 'Easily create budgets.'], ['Keep Track', 'far fa-chart-bar', 'See how much you\'ve spent.'], ['It\'s Flexible', 'fas fa-birthday-cake', 'Plan for any life event.']];
+    let messages = [['Budgets? Yes!', 'fas fa-money-check-alt', 'Easily create budgets.', budget, budgetMB], ['Keep Track', 'far fa-chart-bar', 'See how much you\'ve spent.', track, trackMB], ['It\'s Flexible', 'fas fa-birthday-cake', 'Plan for any life event.', flex, flexMB]];
     let features = messages.map((text, key) => {
         return (
             <div className='main-div' key={key}>
-                <h2>{text[0]}</h2>
-                <i className={text[1]}></i>
-                <p>{text[2]}</p>
+                <div className='left-col'>
+                    <h2>{text[0]}</h2>
+                    <i className={text[1]}></i>
+                    <p>{text[2]}</p>
+                </div>
+                <div className='right-col'>
+                    <img src={text[4]} srcSet={`${text[4]} 300w, ${text[3]} 600w`} />
+                </div>
             </div>
         )
     });
